1c7f300411843885d968ef1f1382254b3c121f3d
Konstantin <dj-ok@mail.ru>
2024-01-23 16:17:42 +0500

    Третье задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,15 @@ public class Main {
         System.out.println(countEvens(new int[] {2, 2, 0}));
         System.out.println(countEvens(new int[] {1, 3, 5}));
         System.out.println("----------");
-        System.out.println("Задание 2");
+        System.out.println("Задание 2:");
         System.out.println(dif(new float[] {2.1f, -1f, 20.2f, -30f, 4.3f}));
         System.out.println(dif(new float[] {20f, -2.4f, 0f}));
         System.out.println(dif(new float[] {1.5f, -3f, 5.6f}));
+        System.out.println("----------");
+        System.out.println("Задание 3:");
+        System.out.println((isDoubleZero(new int[] {2, 1, 2, 3, 4, 2, 2, 0, 1, 3, 5}) ? "Есть два соседних элемента" : "Нет двух соседних элементов") + " с нулевым значением");
+        System.out.println((isDoubleZero(new int[] {2, 1, 2, 3, 4, 2, 0, 0, 1, 3, 5}) ? "Есть два соседних элемента" : "Нет двух соседних элементов") + " с нулевым значением");
+        System.out.println((isDoubleZero(new int[] {2, 0, 2, 3, 4, 2, 2, 0, 1, 3, 5}) ? "Есть два соседних элемента" : "Нет двух соседних элементов") + " с нулевым значением");
     }
 
     //1. Написать метод, возвращающий количество чётных элементов массива.
@@ -24,7 +29,7 @@ public class Main {
     }
 
     //2. Написать функцию, возвращающую разницу между самым большим и самым маленьким элементами переданного не пустого массива.
-    private static Float dif(float[] array){
+    private static float dif(float[] array){
         float min = array[0];
         float max = array[0];
         for (int i = 1; i < array.length; i++) {
@@ -34,5 +39,13 @@ public class Main {
                 max = array[i];
         }
         return max-min;
+    }
+
+    //3. Написать функцию, возвращающую истину, если в переданном массиве есть два соседних элемента, с нулевым значением.
+    private static Boolean isDoubleZero(int[] array){
+        for (int i = 0; i < array.length-1; i++)
+            if ((array[i] == 0) && (array[i+1] == 0))
+                return true;
+        return false;
     }
 }
