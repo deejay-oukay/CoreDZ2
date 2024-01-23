1911d14fe4b0b10ff0ae9cea26c7d5bfda203355
Konstantin <dj-ok@mail.ru>
2024-01-23 13:42:22 +0500

    Первое задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.sql.Array;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println(countEvens(new int[] {2, 1, 2, 3, 4}));
+        System.out.println(countEvens(new int[] {2, 2, 0}));
+        System.out.println(countEvens(new int[] {1, 3, 5}));
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    //Написать метод, возвращающий количество чётных элементов массива.
+    //countEvens([2, 1, 2, 3, 4]) → 3
+    //countEvens([2, 2, 0]) → 3
+    //countEvens([1, 3, 5]) → 0
+    private static int countEvens(int[] array){
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if ((array[i] % 2) == 0)
+                counter++;
         }
+        return counter;
     }
 }
